455ddecab33a64026372f89cd06ad6db85e46abd
Alex Babis <acbabis@gmail.com>
2018-05-31 09:41:30 -0700

    Linting graph

--- src/Graph/Graph.js
+++ src/Graph/Graph.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Graph.css';
 
-export default class Graph extends Component {
-  render() {
-    const {
-      values,
-      minValue = 0,
-      maxValue = 1,
-      maxSize,
-    } = this.props;
+export default (props) => {
+  const {
+    values,
+    minValue = 0,
+    maxValue = 1,
+    maxSize,
+  } = props;
 
-    const zeroes = new Array(maxSize - values.length).fill(0);
-    const valuesList = [...zeroes, ...values];
+  const zeroes = new Array(maxSize - values.length).fill(0);
+  const valuesList = [...zeroes, ...values];
 
-    return (
-      <div className="graph">{
-        valuesList.map((sample, index) => (
-          <div
-            key={index}
-            className="sample"
-            style={{height: Math.floor((sample - minValue) * 100 / maxValue) + '%'}}
-          />))
-      }
-      </div>
-    );
-  }
-}
+  return (
+    <div className="graph">{
+      valuesList.map(sample => (
+        <div
+          key={sample}
+          className="sample"
+          style={{ height: `${(Math.floor((sample - minValue) * 100) / maxValue)}%` }}
+        />))
+    }
+    </div>
+  );
+};
