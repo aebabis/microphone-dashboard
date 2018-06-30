2f91ed6d2532a71376f8feb8bfea964aa7837988
Alex Babis <acbabis@gmail.com>
2018-06-30 09:13:15 -0700

    Showing notes

--- src/PitchChart/PitchChart.js
+++ src/PitchChart/PitchChart.js
@@ -12,9 +12,12 @@ const BAR_RES = 10;
 const HEIGHT = 500;
 
 const NOTES = ['c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'b#'];
-const OCTAVES = [0, 1, 2, 3, 4, 5, 6];
+const MAX_OCTAVE = 6;
+const OCTAVES = new Array(MAX_OCTAVE + 1).fill(null).map((_, i) => i);
 const ALL_NOTES = [].concat(...OCTAVES
   .map(octave => NOTES.map(note => parse(`${note}${octave}`))));
+
+const getOctave = freq => Math.log2(freq) - OCTAVE_OFFSET;
 
 class PitchChart extends Component {
   constructor(props) {
@@ -43,18 +46,28 @@ class PitchChart extends Component {
     canvas.width = width;
     canvas.height = HEIGHT;
 
-    ALL_NOTES.forEach(({ pc, /* oct, */ freq }) => {
+    ALL_NOTES.forEach(({
+      pc,
+      oct,
+      acc,
+      freq,
+    }) => {
       const octave = Math.log2(freq) - OCTAVE_OFFSET;
       const offset = HEIGHT - ((octave * HEIGHT) / 6);
       context.beginPath();
-      context.moveTo(0, offset);
+      context.moveTo(20, offset);
       context.lineTo(width, offset);
       context.strokeStyle = pc === 'C' ? 'rgb(70, 70, 70)' : 'rgb(20, 20, 20)';
       context.stroke();
+
+      if (acc === '') {
+        context.fillStyle = 'rgb(50, 50, 50)';
+        context.fillText(`${pc}${oct}`, 2, offset + 3);
+      }
     });
 
     values.slice().reverse().forEach((value, index) => {
-      const octave = Math.log2(value) - OCTAVE_OFFSET;
+      const octave = getOctave(value);
       const offset = HEIGHT - ((octave * HEIGHT) / 6);
       context.beginPath();
       context.moveTo((maxSize - index) * BAR_RES, offset);
